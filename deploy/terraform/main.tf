01957bbbf21922180002d5c32d12464d6c055d5c
Nivaldo Melo <nivaldogmelo@gmail.com>
2021-10-31 21:06:24 -0300

    deploy: apply terraform fmt

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = "desafio-globo-330323"
+  project = "desafio-globo-330323"
 }
 
 resource "google_compute_instance" "comment_api" {
@@ -24,7 +24,7 @@ resource "google_compute_instance" "comment_api" {
   }
 
   metadata = {
-    ssh-keys= "ansible:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJdILTA3DFea0ukS2Gxk8k2cLW8eLSdLr5NokJECCrjlSw7Mp2lmMBrt2ZsH2Ofh2nyJmVb1WiUfhK5dITlam7UCn8gbTFHHAb1DtT4dw2BkAGRo/mFv4F8Szir4G9/DdXqa816uLkPsV7M0cFVmUmyWOqu0b0t88BJjKqPBG5TBOtRbGiut45ilfq25X27uq6Vz995Gank6Q87CxGVE0hz85IGeaTNWOr80U8CbWrgP3lmhtYG3xiODd4OxCAp2dUxdUUlUKhHoSMggga2W+F/VxTnGmzinAStjUARr8macCHssMu8tmCOymztdn/KCfHGcLSyGlS//D8P7IhpA3Xr92yBoAA4kGxSX7/aI18WCZnckz0+0Nrj3gixcHwUEC74TEG3jSyOK57QDwMkVUJawNPOCma2GlFcRc1mOBi71/jGKdlBRmjDPBScI5NXZWHQgJ9L1dTGffjlWvmCJtr/JyIamlX8oYQYTLAp7Dt84/q+C5phRXnGwSRwb1XIb8= ansible"
+    ssh-keys = "ansible:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJdILTA3DFea0ukS2Gxk8k2cLW8eLSdLr5NokJECCrjlSw7Mp2lmMBrt2ZsH2Ofh2nyJmVb1WiUfhK5dITlam7UCn8gbTFHHAb1DtT4dw2BkAGRo/mFv4F8Szir4G9/DdXqa816uLkPsV7M0cFVmUmyWOqu0b0t88BJjKqPBG5TBOtRbGiut45ilfq25X27uq6Vz995Gank6Q87CxGVE0hz85IGeaTNWOr80U8CbWrgP3lmhtYG3xiODd4OxCAp2dUxdUUlUKhHoSMggga2W+F/VxTnGmzinAStjUARr8macCHssMu8tmCOymztdn/KCfHGcLSyGlS//D8P7IhpA3Xr92yBoAA4kGxSX7/aI18WCZnckz0+0Nrj3gixcHwUEC74TEG3jSyOK57QDwMkVUJawNPOCma2GlFcRc1mOBi71/jGKdlBRmjDPBScI5NXZWHQgJ9L1dTGffjlWvmCJtr/JyIamlX8oYQYTLAp7Dt84/q+C5phRXnGwSRwb1XIb8= ansible"
   }
 }
 
--- deploy/terraform/output.tf
+++ deploy/terraform/output.tf
@@ -1,4 +1,4 @@
 output "comment_api_ip" {
-    value = google_compute_instance.comment_api.network_interface.0.access_config.0.nat_ip
-    description = "The VM machine external IP"
+  value       = google_compute_instance.comment_api.network_interface.0.access_config.0.nat_ip
+  description = "The VM machine external IP"
 }
